fb7e40770e073e59c091e1e3fe2f8f372acc1bd4
ACO-626 <ocampoalejandro.oc@gmail.com>
2022-09-27 17:26:44 -0500

    Comunicación Serial establecida

---
+++ ContadorVehicular/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace ContadorVehicular
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comLabel
+            // 
+            this.comLabel.AutoSize = true;
+            this.comLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comLabel.Location = new System.Drawing.Point(367, 158);
+            this.comLabel.Name = "comLabel";
+            this.comLabel.Size = new System.Drawing.Size(86, 31);
+            this.comLabel.TabIndex = 0;
+            this.comLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label comLabel;
+    }
+}
+
---
+++ ContadorVehicular/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ContadorVehicular
+{
+    public partial class Form1 : Form
+    {
+
+        bool endConection = false; //Bandera de conexión
+        System.IO.Ports.SerialPort puerto = new System.IO.Ports.SerialPort(); //Instancia de la clase puerto
+
+        public Form1()
+        {
+            InitializeComponent();
+            puerto.PortName = "COM4";  //Indicación de puerto por defecto
+            puerto.BaudRate = 9600;    //Indicación de la velocidad de comunicación 
+            puerto.ReadTimeout = 500;  //Tiempo de espera para periodo de espera en lectura
+            try
+            {
+                puerto.Open();
+            }
+            catch { }
+            
+        }
+
+        private void EscuchaPuerto()
+        {
+            while(!endConection)
+            {
+                try
+                {
+                    string cadena = puerto.ReadLine();
+                    comLabel.Invoke(new MethodInvoker(
+                        delegate
+                        {
+                            comLabel.Text = cadena;
+                        }
+                        ));
+                    
+                }
+                catch { }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread Hilo = new Thread(EscuchaPuerto);
+            Hilo.Start();
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            endConection = true;
+            if (puerto.IsOpen)
+            {
+                puerto.Close();
+            }
+        }
+    }
+}
